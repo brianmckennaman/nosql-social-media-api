d0bd2839cc29e2f80d680aed387318525dc53c31
Brian McKenna <brianmckennaman@gmail.com>
2022-11-10 16:36:06 -0500

    fixed adding reactions

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
     {
@@ -20,12 +21,7 @@ const thoughtSchema = new Schema(
             type: String,
             required: true,
         },
-        reactions: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Reaction'
-            },
-        ],
+        reactions: [reactionSchema],
     },
     {
         toJSON: {
